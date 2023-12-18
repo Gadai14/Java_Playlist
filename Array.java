125a17465105d0dd286382119cf74ee4594d413f
Gadai14 <biswajit1973.pal@gmail.com>
2023-12-18 09:41:00 +0530

    new

---
+++ Array.java
@@ -0,0 +1,17 @@
+
+public class Array {
+    public static void main(String[] args) {
+        int nums[]=new int[4];
+        nums[0]=34;
+        nums[1]=90;
+        nums[2]=99;
+        nums[3]=56;
+        System.out.println("The Array is : ");
+        for(int i=0;i<3;i++)
+        {
+            
+        System.out.println(nums[i]);
+        }
+    }
+}
+
